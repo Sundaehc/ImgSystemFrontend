5d9f6a7f3616406b16a56af69804d1b207f8edbf
Sundaehc <1156050293@qq.com>
2025-03-28 17:56:16 +0800

    图片拖拽上传

--- src/pages/Admin/Image/index.tsx
+++ src/pages/Admin/Image/index.tsx
@@ -152,10 +152,9 @@ const ImageUpload: React.FC = ({}) => {
             <p className="ant-upload-drag-icon">
               <InboxOutlined />
             </p>
-            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            <p className="ant-upload-text">拖拽或点击上传</p>
             <p className="ant-upload-hint">
-              Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-              banned files.
+              只允许上传图片文件！支持单张或多张上传
             </p>
             {/*<Upload*/}
             {/*  name="file"*/}
